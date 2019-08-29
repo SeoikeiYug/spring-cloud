090b85f6d7f2aa4e1bb2288114f1fb1a03fa47d8
Yu Shuai Qi <shuaiqi@ifastfinancial.com.cn>
2019-08-29 15:40:58 +0800

    initial

---
+++ spring-cloud-eureka-provider/src/main/java/com/ysq/example/provider/SpringCloudEurekaProviderApplication.java
@@ -0,0 +1,20 @@
+package com.ysq.example.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class SpringCloudEurekaProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudEurekaProviderApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String home() {
+		return "hello world";
+	}
+}
