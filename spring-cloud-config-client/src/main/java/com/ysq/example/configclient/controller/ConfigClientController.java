88eeb1d4590de11a2ea3e75c957182b7f6c046d8
Yu Shuai Qi <shuaiqi@ifastfinancial.com.cn>
2019-09-05 17:35:50 +0800

    config server client

---
+++ spring-cloud-config-client/src/main/java/com/ysq/example/configclient/controller/ConfigClientController.java
@@ -0,0 +1,18 @@
+package com.ysq.example.configclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConfigClientController {
+
+	@Value("${content}")
+	private String content;
+
+	@GetMapping("/")
+	public String content() {
+		return content;
+	}
+
+}
